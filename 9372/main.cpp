7adc82427d8ad6f78379237e5e698c076b0ec791
snowrabbit123@naver.com <snowrabbit123@naver.com>
2024-11-04 16:43:17 +0900

    9372

--- 9372/main.cpp
+++ 9372/main.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    int test_case = 0;
+    cin >> test_case;
+
+    for (int i = 0; i < test_case; i++)
+    {
+        int n, m; // n: 국가의 수, m: 비행기의 수
+
+        cin >> n >> m;
+
+        for (int j = 0; j < m; j++) 
+        {
+            int a, b;
+            cin >> a >> b;
+        }
+
+        cout << n - 1 << endl;
+    }
+
+    return 0;
+}
